48ae8424cdea5dd28c7b87bc4572a2b290ee77b0
Kevin M Porter <kevin@kevinporter.me>
2015-11-09 02:21:26 -0500

    Yolo working

--- Project 2/kevinporter_project2.py
+++ Project 2/kevinporter_project2.py
@@ -8,6 +8,7 @@ import ast
 import itertools
 import re
 import sys
+import random
 
 
 class Program(object):
@@ -19,8 +20,29 @@ class Program(object):
         self.variables = {}
 
     def error(self, s):
-        print('ERROR: ' + s + ' [Line ' + str(self.line_ptr + 1) + ']')
+        print('ERROR: ' + s + ' [Line ' + str(self.line_ptr + 1) + ']'),
         return
+
+
+    def addition(self, scanner, token):
+        values = token.split('+')
+        values = [i.replace(' ', '') for i in values]
+        for k, value in enumerate(values):
+            try:
+                values[k] = int(value)
+            except:
+                try:
+                    is_literal = re.match(r'(\'(.*)\')', value)
+                    groups = filter(None, is_literal.groups())
+                    if len(groups) > 0:
+                        values[k] = str(value)
+                except:
+                    try:
+                        values[k] = self.variables[value]
+                    except:
+                        self.error('Bad addition.')
+        value = values[0] + values[1]
+        return value
 
 
     def comment(self, scanner, token):
@@ -33,18 +55,42 @@ class Program(object):
         name = assignment.pop(0)
         assignment.pop(0)
         value = ' '.join(assignment)
-        is_list = re.match(r'(\s*\[(\s*(\b.*\b),\s*)*\])', value)
+        is_list = re.match(r'((\s*\[(\s*(((.*))|([0-9]*)),\s*)*\]))', value)
         try:
             groups = filter(None, is_list.groups())
             if len(groups) > 0:
                 try:
-                    value = ast.literal_eval(value)
+                    items = value
+                    variables = items.replace('[', '')
+                    variables = variables.replace(']', '')
+                    variables = variables.replace(',', '')
+                    variables = re.findall(r'(?<![\S"])([^\'\s]+)(?![\S"])', variables)
+                    for i in variables:
+                        items = items.replace(i + ',', '')
+                    items = ast.literal_eval(items)
+                    for i in variables:
+                        try:
+                            items.append(self.variables[i])
+                        except:
+                            self.error('Bad reference.')
+                    value = items
                 except:
                     self.error('Incorrectly formatted list.')
                     return
         except:
-            pass
-        self.variables[name] = value
+            is_literal = re.match(r'^\s*((\'(.*)\')|([0-9]*))\s*$', value)
+            if is_literal is not None:
+                groups = filter(None, is_literal.groups())
+            else:
+                try:
+                    value = self.parse(value)[0][0]
+                except:
+                    self.error('Function not found.')
+                    return
+        try:
+            self.variables[name] = int(value)
+        except:
+            self.variables[name] = value
 
 
     def end_loop(self, scanner, token):
@@ -55,19 +101,57 @@ class Program(object):
         self.looping = True
         instructions = token.split()
         variable = instructions[1]
-        iteritem = self.variables[instructions[3]]
+        try:
+            iteritem = self.variables[instructions[3]]
+        except:
+            try:
+                iteritem = int(iteritem)
+            except:
+                self.error('Bad loop parameters.')
+                return
         self.line_ptr = self.line_ptr + 1
         begin_at = self.line_ptr
         nastiness = list(enumerate(self.file))
-        for i in iteritem:
-            self.variables[variable] = i
-            for line_ptr, line in itertools.islice(nastiness, begin_at, len(nastiness)):
-                if self.looping:
-                    self.line_ptr = line_ptr
-                    self.parse(line)
-                else:
-                    break
-            self.looping = True
+        if isinstance(iteritem, int):
+            for i in range(iteritem):
+                self.variables[variable] = i
+                for line_ptr, line in itertools.islice(nastiness, begin_at, len(nastiness)):
+                    if self.looping:
+                        self.line_ptr = line_ptr
+                        self.parse(line)
+                    else:
+                        break
+                self.looping = True
+        else:
+            for i in iteritem:
+                self.variables[variable] = i
+                for line_ptr, line in itertools.islice(nastiness, begin_at, len(nastiness)):
+                    if self.looping:
+                        self.line_ptr = line_ptr
+                        self.parse(line)
+                    else:
+                        break
+                self.looping = True
+
+    def multiplication(self, scanner, token):
+        values = token.split('*')
+        values = [i.replace(' ', '') for i in values]
+        for k, value in enumerate(values):
+            try:
+                values[k] = int(value)
+            except:
+                try:
+                    is_literal = re.match(r'(\'(.*)\')', value)
+                    groups = filter(None, is_literal.groups())
+                    if len(groups) > 0:
+                        values[k] = str(value)
+                except:
+                    try:
+                        values[k] = int(self.variables[value])
+                    except:
+                        self.error('Bad multiplication.')
+        value = values[0] * values[1]
+        return value
 
 
 
@@ -75,15 +159,94 @@ class Program(object):
         token = token.split()
         token.pop(0)
         value = ' '.join(token)
-        is_literal = re.match(r'(\s*\[(\s*(\b.*\b),\s*)*\])|(\'(.*)\')|([0-9])*', value)
+        is_literal = re.match(r'(\s*\[(\s*(\b.*\b),\s*)*\])|(\'(.*)\')|([0-9]*)', value)
         groups = filter(None, is_literal.groups())
         if len(groups) > 0:
-            print(groups[0])
+            out = str(groups[0])
+            out = out.replace('\'', '')
+            out = out.replace('\\n', '\n')
+            print(out),
         else:
             try:
-                print(self.variables[value])
+                if '+' in value:
+                    values = value.split('+')
+                else:
+                    values = [value, ]
+                for value in values:
+
+                    item = value.split('[')
+                    if len(item) > 1:
+                        index = item[1].replace(']', '')
+                        variable = item[0]
+                        try:
+                            index = int(index)
+                        except:
+                            index = int(self.variables[index])
+                        out = self.variables[variable][index]
+                        out = out.replace('\'', '')
+                        sys.stdout.write(out)
+                    else:
+                        out = str(self.variables[item[0]])
+                        out = out.replace('\'', '')
+                        sys.stdout.write(out)
             except:
-                self.error('Bad reference')
+                self.error('Bad reference.')
+
+
+    def random(self, scanner, token):
+        func, args = token.split('(')
+        args = args.replace(')', '')
+        args = args.replace(' ', '')
+        args = args.split(',')
+        if len(args) == 2:
+            try:
+                val = random.randrange(int(args[0]), int(args[1]))
+            except:
+                self.error('random() expected two integers. Given at least one string.')
+        elif len(args) == 1:
+            try:
+                val = random.randrange(0, int(args[0]))
+            except:
+                val = random.choice(self.variables[args[0]])
+        else:
+            self.error('Improper use of random().')
+            return
+        return val
+
+    def shuffle(self, scanner, token):
+        func, args = token.split('(')
+        args = args.replace(')', '')
+        args = args.replace(' ', '')
+        args = args.split(',')
+        if len(args) == 2:
+            pass
+        elif len(args) == 1:
+            val = self.variables[args[0]]
+            random.shuffle(val)
+        else:
+            self.error('Improper use of shuffle().')
+            return
+        return val
+
+    def subtraction(self, scanner, token):
+        values = token.split('-')
+        values = [i.replace(' ', '') for i in values]
+        for k, value in enumerate(values):
+            try:
+                values[k] = int(value)
+            except:
+                try:
+                    is_literal = re.match(r'(\'(.*)\')', value)
+                    groups = filter(None, is_literal.groups())
+                    if len(groups) > 0:
+                        values[k] = str(value)
+                except:
+                    try:
+                        values[k] = self.variables[value]
+                    except:
+                        self.error('Bad subtraction.')
+        value = values[0] - values[1]
+        return value
 
 
     def parse(self, line):
@@ -91,14 +254,19 @@ class Program(object):
         variable_declarator = r'(\s*\bswag\b)'
         variable = r'(\s*(\b[_A-Za-z]+[_A-Za-z0-9]*\b))'
         assignment_operator = r'\s*(=)'
-        value = r'(\s*(\'*[^\[](\w+)\'*[^\[]))'
+        value = r'(\s*(\'{0,1}[^\[](.*)\'{0,1}[^\[]))'
         list_obj = r'(\s*\[(\s*(\b.*\b),\s*)*\])'
         print_command = r'^(\s*\bholla\b)'
         ratchet = r'(\s*\b(ratchet)\b)'
         on_fleek = r'(\s*\b(on_fleek)\b)'
+        random_r = r'(\s*\b(random)\((([0-9]+\s*(,\s*[0-9]+){0,1})|(' + variable + r'))\))'
+        shuffle_r = r'(\s*\b(shuffle)\((([0-9]+\s*(,\s*[0-9]+){0,1})|(' + variable + r'))\))'
         loop = r'(\s*\b(naenae)\b\s+(' + variable + r')\s+(in)(' + variable + r'))'
         end_loop = r'\s*\b(groovy)\b'
         variable_cu = (variable_declarator + r'*' + variable + assignment_operator + r'|'.join([value, ]))
+        addition = r'(' + r'|'.join([variable, value]) + r')\s*\+\s*(' + '|'.join([variable, value]) + r')'
+        subtraction = r'(' + r'|'.join([variable, value]) + r')\s*\-\s*(' + '|'.join([variable, value]) + r')'
+        multiplication = r'(' + r'|'.join([variable, value]) + r')\s*\*\s*(' + '|'.join([variable, value]) + r')'
         scanner = re.Scanner([
                               (comment, self.comment),
                               (variable_cu, self.create_update_variable),
@@ -107,11 +275,17 @@ class Program(object):
                               (print_command + list_obj, self.print_command),
                               (loop, self.loop),
                               (end_loop, self.end_loop),
+                              (random_r, self.random),
+                              (shuffle_r, self.shuffle),
+                              (addition, self.addition),
+                              (subtraction, self.subtraction),
+                              (multiplication, self.multiplication),
                               (r'\s*', None),
                               (r'\z', None),
         ])
 
         result = scanner.scan(line)
+        return result
 
     def interpret(self):
         for line_ptr, line in enumerate(self.file):
